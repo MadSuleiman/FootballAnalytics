16ff0dab2da00f48bf0aee001510b0871757b5ae
Ahmad Suleiman <ahmadsuleimanbusiness@gmail.com>
2022-11-02 13:05:05 -0700

    Created SQL file for create table statements

---
+++ FootProj/create_table.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+
+engine = create_engine("sqlite:////main.db", echo = True)
+meta = MetaData()
+
+
+students = Table(
+   'students', meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('name', String), 
+   Column('lastname', String), 
+)
+
+meta.create_all(engine)
--- FootProj/main.py
+++ FootProj/main.py
@@ -1,1 +1,15 @@
-print("")
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+
+engine = create_engine("sqlite:////main.db")
+
+
+
+
+db.create_all()
+db.session.commit()
