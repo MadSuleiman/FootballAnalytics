383a2eeba5e7f50aa7d67fb21c97ea7e912f46a2
Ahmad Suleiman <ahmadsuleimanbusiness@gmail.com>
2022-11-02 13:57:54 -0700

    Added table properties

--- FootProj/createTables.sql
+++ FootProj/createTables.sql
@@ -9,11 +9,29 @@ DROP TABLE players2positions;
 DROP TABLE teams2games;
 
 CREATE TABLE players(
-    playerID int PRIMARY key not NULL
+    playerID int PRIMARY key not NULL,
+    teamID int
 );
 
 CREATE TABLE stats(
-    playerID int not NULL
+    playerID int not NULL,
+    pass_cmp int default 0,
+    pass_att int default 0,
+    pass_yds int default 0,
+    pass_td int default 0,
+    pass_int int default 0,
+    pass_sacked int default 0,
+    pass_sacked_yds int default 0,
+    pass_long int default 0,
+    pass_rating int default 0,
+    rush_att int default 0,
+    rush_yds int default 0,
+    rush_td int default 0,
+    rush_long int default 0,
+    targets int default 0,
+    rec int default 0,
+    rec_yds int default 0,
+    rec_td int default 0
 );
 
 CREATE TABLE positions(
@@ -21,30 +39,41 @@ CREATE TABLE positions(
 );
 
 CREATE TABLE teams(
-    teamID int PRIMARY key not NULL
-
+    teamID int PRIMARY key not NULL,
+    name string, 
+    city string,
+    color1 string,
+    color2 string
 );
 
 CREATE TABLE divisions(
-    divisionID int PRIMARY key not NULL
+    divisionID int PRIMARY key not NULL,
+    teamID int not NULL
 );
 
 CREATE TABLE coaches(
-    coachID int PRIMARY key not NULL
+    coachID int PRIMARY key not NULL,
+    name string,
+    age int,
+    winPercentage int
 );
 
 CREATE TABLE games(
-    gameID int PRIMARY key not NULL
+    gameID int PRIMARY key not NULL,
+    score int,
+    date dateTime 
 );
 
 -- Our first many to many table
 CREATE TABLE players2Positions(
     playerID int not NULL,
-    positionID int not NULL
+    positionID int not NULL,
+    year dateTime
 );
 
 -- Our second many to many table
 CREATE TABLE teams2Games(
-    teamID int not NULL,
+    team1ID int not NULL,
+    team2ID int not NUll,
     gameID int not NULL
 );
