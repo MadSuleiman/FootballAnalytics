9848b089cadf093997db95ae856fcffb0208eb7e
lalakerspro <vvyas@ucmerced.edu>
2022-11-09 14:37:03 -0800

    12

--- FootProj/queryTables.sql
+++ FootProj/queryTables.sql
@@ -140,4 +140,23 @@ select t1.name, t1.wins, t2.losses from
 
         group by t_team
     ) as t2  
-    on (t1.name = t2.name)
+    on (t1.name = t2.name);
+
+SELECT'________________________________________________________________________';
+SELECT'12. For coaches hired before 2010, list passing yards by their QB in descending order, with their team name';
+SELECT'________________________________________________________________________';
+SELECT c_name, MAX(yds), c_team FROM(
+select c_name, ROUND(avg(pass_yds),2) as yds, c_team
+    from players, players2positions, positions, stats, coaches, teams
+    where players.id = players2positions.playerID 
+    and players2positions.playerID = stats.playerID
+    and positionID = positions.id
+    and position = "QB"
+	and players.p_team=coaches.c_team
+	and coaches.c_year_hired<=2010
+    group by p_name
+    ORDER BY yds DESC
+)
+
+
+;
